efb4ecf490f957d266e74f0864b00ffa51b9cc11
Francis19 <9alemanbravo@gmail.com>
2025-04-29 12:39:10 -0500

    Modificacion desde VSCode

---
+++ sentenciascontrol.py
@@ -0,0 +1,13 @@
+#sentencias de control
+#Ejemplo de if
+if True:
+    print("La condicion es verdadera")
+else:
+    print("La condicion es falsa")
+#Ejemplo de if con else if
+if True:
+    print("La condicion es verdadera")
+elif False:
+    print("La condicion es falsa")
+else:
+    print("La condicion es falsa")
